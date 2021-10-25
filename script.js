e9c23b6776a6aa73187daad02020006a3ea8d383
【ƬᗩǤ】fred oliveira <joyjoy54326@gmail.com>
2021-10-25 07:32:12 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector(".menu_mobile");
+let nav = document.getElementById("nav_menu");
+
+menu.addEventListener("click", function(e){
+    nav.classList.toggle("active")
+
+})
+    
